8105d12e227152389d3dddfddef6f6224e3bf11d
Zerkk <32021463+Zerkk@users.noreply.github.com>
2018-03-13 10:52:00 -0500

    Add files via upload

---
+++ API/database/migrations/2018_03_13_061024_create_drivers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('sponsor');
+            $table->integer('score');
+            $table->SoftDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('drivers');
+    }
+}
